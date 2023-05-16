84302e8b08de72785cfc99d3b96936166ef20be5
Sean Ang <seanang@161.131.189.118.static.m1net.com.sg>
2023-05-16 11:19:36 +0800

    Exclude playwright.config.ts from tsconfig.json

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "mjs"],
+  roots: ["<rootDir>/src"],
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+  ],
+};
